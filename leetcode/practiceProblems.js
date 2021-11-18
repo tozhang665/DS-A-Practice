c84a7285b1ec4e57a36657495ef298bc15d70e56
Tony Zhang <tozhang665@gmail.com>
2021-11-18 13:32:40 -0800

    setup shortestPath

---
+++ leetcode/practiceProblems.js
@@ -0,0 +1,19 @@
+console.log("hello")
+
+const edges = [
+  ['w', 'x'],
+  ['x', 'y'],
+  ['z', 'y'],
+  ['z', 'v'],
+  ['w', 'v']
+];
+
+
+const shortestPath = (edges, nodeA, nodeB) => {
+
+};
+
+
+
+
+shortestPath(edges, 'w', 'z'); // -> 2
