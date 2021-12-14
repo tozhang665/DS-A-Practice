ddcc16dae6186f017ea0127f5c87aa3fff7e971f
Tony Zhang <tozhang665@gmail.com>
2021-12-13 20:11:24 -0800

    added some promise practice

--- javascript/closure.js
+++ javascript/closure.js
@@ -1,14 +1,87 @@
-function soundMaker(sound,times){
-
-  function makeSound(){
-    console.log(sound);
+let myPromise = new Promise((res,rej)=>{
+  let x = 4
+  if(x === 5){
+    res("hello")
+  }else{
+    rej("bye")
   }
+})
 
-
-  for(let i = 0; i < times; i ++){
-    makeSound();
-  }
+let fetch2 = (stringHere)=>{
+  return new Promise((resolve,reject)=>{
+    if(stringHere === "hello"){
+      resolve("HIHIHIH")
+    }
+    else{
+      reject("BYBYBYB")
+    }
+  })
 }
 
 
-soundMaker("woof",5)
+
+//run the 3 promises, and return whichever one fails.
+
+// fetch2("hello").then((message)=>{
+//   console.log("THIS IS FETCH 2")
+//   console.log(message)})
+//   .catch((failure)=>{
+//     console.log("THIS IS THE FAIL FOR FETCH 2")
+//     console.log(failure)
+//   })
+  
+//   myPromise
+//   .then((message)=>{
+//     console.log("this succeeded")
+//     console.log(message)})
+//   .catch((message)=>
+//   {
+//     console.log("this failed")
+//     console.log(message)}
+//   )
+let myFail = ""
+
+myPromise.catch((message)=> {
+  console.log(typeof(message))
+  myFail = message
+  console.log(myFail)
+})
+console.log(myFail)
+
+fetch2("hell").catch((message)=> myFail += message)
+
+
+console.log(myFail)
+
+
+import react from React
+import child from "./sdafaksljhf"
+
+class Todo extends react.Component{
+  constructor(props) {
+    super(props)
+    this.state ={
+        "person1": ["todo1","todo2","todo3"],
+        "person2": ["todo1","todo2"]
+    }
+  }
+
+  ComponentDidMount(){
+    
+  }
+  
+
+  render(){
+    return(
+
+      <div>
+        {this.state.map((ele,idx)=>{
+          <child person={ele} key={idx}/>
+        })}
+      </div>
+    )
+  }
+
+}
+
+export default Todo
