21bd1b6dd4260686503e0ee5166849655d385a85
Tony Zhang <tozhang665@gmail.com>
2022-03-12 18:21:47 -0800

    added threesum

--- leetcode/practiceProblems.js
+++ leetcode/practiceProblems.js
@@ -189,3 +189,35 @@ const allTreePaths = (root) => {
 
 
 
+
+
+var threeSum = function(nums) {
+  let ans= []
+  nums = nums.sort((a,b)=>a-b)
+  for(let i = 0; i < nums.length - 2;i++){
+      if(nums[i] > 0) break;
+      if(nums[i] === nums[i-1]) continue;
+      let left = i+1;
+      let right = nums.length-1;
+      while(left < right){
+          let sum = nums[i] + nums[left] + nums[right]
+          if(sum < 0){
+              left++
+          }else if(sum > 0){
+              right--
+          }else{
+              ans.push([nums[i],nums[left],nums[right]])
+              
+              
+              while(left < right && nums[left] === nums[left+1]) left++
+              while(left < right && nums[right] === nums[right-1]) right--
+              left++
+              right--
+          }
+          
+      }
+  }
+  
+  
+  return ans
+};
